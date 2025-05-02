0540c9eabde9d7870bd0feeef73827b19b9d0cd4
srini255 <srinitamil17@gmail.com>
2025-05-03 01:49:46 +0530

    added error middleware to handle Mangodb errors

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
 import express from 'express';
 
+import cookieParser from 'cookie-parser';
+
 import {PORT} from './config/config.js'
 import userRouter from './routes/user.routes.js';
 import authRouter from './routes/auth.routes.js';
 import subscriptionRouter from './routes/subscriptions.routes.js';
 import connectToDatabase from './Database/mongodb.js';
+import errorMiddleware from './middlewares/error.middleware.js';
+
 
 const app=express();
+
+
+app.use(express.json()); //handle json data
+app.use(express.urlencoded({extended:true})); //handle form data send via html forms
+app.use(cookieParser()); //handle cookies
 
 app.use('/api/v1/auth',authRouter);
 app.use('/api/v1/users',userRouter);
 app.use('/api/v1/subscriptions',subscriptionRouter);
+app.user(errorMiddleware);
 
 app.get('/',(req,res)=>{
     res.send('welcome to the subscription tracker api');
